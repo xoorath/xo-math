34ee76d07e0f4882c6ccb0364ed5e07e880118f8
xoorath <jared@xoorath.com>
2016-09-08 13:15:25 -0400

    including appropriate legal for quat lerp

--- xo-math/include/QuaternionMethods.h
+++ xo-math/include/QuaternionMethods.h
@@ -208,6 +208,25 @@ void Quaternion::LookAtFromDirection(const Vector3& direction, Quaternion& outQu
 }
 
 void Quaternion::Slerp(const Quaternion& a, const Quaternion& b, float t, Quaternion& outQuat) {
+    //      The folowing copyright and licence applies to the contents of this Quaternion::Slerp method
+
+    //      Copyright 2013 BlackBerry Inc.
+    //      Licensed under the Apache License, Version 2.0 (the "License");
+    //      you may not use this file except in compliance with the License.
+    //      You may obtain a copy of the License at
+    //      http://www.apache.org/licenses/LICENSE-2.0
+    //      Unless required by applicable law or agreed to in writing, software
+    //      distributed under the License is distributed on an "AS IS" BASIS,
+    //      WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    //      See the License for the specific language governing permissions and
+    //      limitations under the License.
+
+    //      Original file from GamePlay3D: http://gameplay3d.org
+
+    //      Modified by Jared Thomson in 2016:
+    //        - removed assertions
+    //        - made compatable with xo-math
+
     // Fast slerp implementation by kwhatmough:
     // It contains no division operations, no trig, no inverse trig
     // and no sqrt. Not only does this code tolerate small constraint
