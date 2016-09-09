f79799b460ec035073aec003a44c25828f20eaa2
xoorath <jared@xoorath.com>
2016-09-09 00:38:07 -0400

    pointing doxygen to the doxyfile [ci skip]

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ sys.path.append( "./" )
 read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
 
 if read_the_docs_build:
-    subprocess.call('doxygen Doxyfile', shell=True)
+    subprocess.call('doxygen ./Doxyfile', shell=True)
 
 # -- General configuration ------------------------------------------------
 
