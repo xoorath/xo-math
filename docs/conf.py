2300f4a3c70ee2fa6a7159c8f8792c2e67651fd6
xoorath <jared@xoorath.com>
2016-09-09 07:03:40 -0400

    getting retcode for subprocess call [ci skip]

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,7 @@ read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
 
 if read_the_docs_build:
     try:
-        subprocess.call('doxygen', shell=True)
+        retcode = subprocess.call('doxygen', shell=True)
         if retcode < 0:
             sys.stderr.write("doxygen terminated by signal %s" % (-retcode))
     except OSError as e:
