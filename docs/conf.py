2a6d047bb68fd9adf1081b77d8c133079da5f9ba
xoorath <jared@xoorath.com>
2016-09-08 21:04:03 -0400

    importing sphinx_rtd_theme

--- docs/conf.py
+++ docs/conf.py
@@ -20,6 +20,8 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+import sphinx_rtd_theme
+
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
