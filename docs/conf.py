9591964f7d7a8b6c7cea5ac26dcb2897544777c7
xoorath <jared@xoorath.com>
2016-09-09 00:34:12 -0400

    calling doxygen from readthedocs [ci skip]

--- docs/conf.py
+++ docs/conf.py
@@ -16,13 +16,18 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import os
+import subprocess, os
 import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
 import sphinx_rtd_theme
 
 sys.path.append( "./" )
+
+read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
+
+if read_the_docs_build:
+    subprocess.call('doxygen Doxyfile', shell=True)
 
 # -- General configuration ------------------------------------------------
 
