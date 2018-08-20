8a1796bd28e8b3febcf3513fd13d9eec2dbaea66
xoorath <jared@xoorath.com>
2018-08-20 19:47:19 -0400

    cube demo now demos cubes (plural)

--- demos/demo-00-common/demo-scene.h
+++ demos/demo-00-common/demo-scene.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <xo-math.h>
 #include <vector>
+#include <algorithm>
 
 namespace xo {
 
@@ -26,6 +27,16 @@ struct Transformation {
 
 #undef ENCAPSULATE_TRANSFORM
 
+    inline size_t CountDepth() {
+        size_t count = 0;
+        Transformation* p = m_Parent;
+        while (p) {
+            p = p->m_Parent;
+            count++;
+        }
+        return count;
+    }
+
 private:
     enum {
         SCALE_DIRTY = 1,
@@ -49,7 +60,6 @@ private:
         AVector3 m_Translation = AVector3::Zero;
         AVector3 m_Rotation = AVector3::Zero;
     } m_Values;
-
 
     bool ScaleDirty(bool parentKnownDirty = false) const         { return (m_Flags & SCALE_DIRTY)        || parentKnownDirty ? true : ParentDirty(); }
     bool TranslationDirty(bool parentKnownDirty = false) const   { return (m_Flags & TRANSLATION_DIRTY)  || parentKnownDirty ? true : ParentDirty(); }
@@ -100,11 +110,56 @@ public:
     }
 };
 
+// Wrap Transformation for now. When we move to a DOD approach this will wrap access to the the SOA
+struct SceneObject {
+    SceneObject(Transformation* t) :m_Transformation(t){}
+
+    inline void SetParent(SceneObject const& so) {
+        m_Transformation->SetParent(so.m_Transformation);
+    }
+
+#define ENCAPSULATE_SCENE_OBJECT(namedType) \
+    inline AVector3 const& GetLocal##namedType () { return m_Transformation->GetLocal##namedType(); } \
+    inline void SetLocal##namedType (AVector3 const& v) { m_Transformation->SetLocal##namedType(v); } \
+    inline AVector3 const& GetWorld##namedType () { m_Transformation->GetWorld##namedType(); } \
+    inline void SetWorld##namedType (AVector3 const& v) { m_Transformation->SetWorld##namedType(v); }
+
+    ENCAPSULATE_SCENE_OBJECT(Scale);
+    ENCAPSULATE_SCENE_OBJECT(Translation);
+    ENCAPSULATE_SCENE_OBJECT(Rotation);
+#undef ENCAPSULATE_SCENE_OBJECT
+
+    inline size_t CountDepth() const { return m_Transformation->CountDepth(); }
+
+    AMatrix4x4 const& GetModelNoUpdate() const { return m_Transformation->GetModelNoUpdate(); }
+    AMatrix4x4 const& GetModel() { return m_Transformation->GetModel(); }
+    void Update() { m_Transformation->Update(); }
+    void Destroy() { delete m_Transformation; m_Transformation = nullptr; }
+
+private:
+    Transformation * m_Transformation;
+};
+
 struct Scene {
-    std::vector<Transformation*> m_Transformations;
+    std::vector<SceneObject> m_SceneObjects;
+
+    SceneObject NewSceneObject() {
+        Transformation* t = new Transformation();
+        m_SceneObjects.emplace_back(t);
+        return { t };
+    }
 
     void Update() {
+        for (auto& so : m_SceneObjects) {
+            so.Update();
+        }
+    }
 
+    Scene() = default;
+    ~Scene() {
+        for (auto& so : m_SceneObjects) {
+            so.Destroy();
+        }
     }
 };
 
--- demos/demo-01-cubes/main.cpp
+++ demos/demo-01-cubes/main.cpp
@@ -9,6 +9,8 @@
 using namespace xo;
 
 #define LimitedLog(fmt, ...) { static int limiter = 16; if(limiter > 0) { Log(fmt __VA_ARGS__); limiter--;} }
+// both positive and negative numbers of cubes on each axis (-x to x)
+constexpr int CUBES_XYZ[3] = {5, 5, 5};
 
 class CubesApplication : public Application {
     AMatrix4x4 m_Proj = AMatrix4x4::Identity;
@@ -22,32 +24,53 @@ class CubesApplication : public Application {
     AVector3 m_CamTarget = AVector3::Zero;
     AVector3 m_CamUp = AVector3::Up;
 
-    Transformation m_Cube1, m_Cube2;
+    Scene m_Scene;
+    SceneObject m_Root;
+    std::vector<SceneObject> m_Cubes;
 
     float m_CameraFOV = 45.f;
 
     float m_CubeRotationSpeed = 1.f;
     bool m_SpinCube = false;
+
+    float m_AppTime = 0.f;
 public:
     CubesApplication()
     : Application()
-    , m_CameraFOV(45.f) {
-        m_Cube2.SetParent(&m_Cube1);
+    , m_CameraFOV(45.f) 
+    , m_Root(nullptr) {
+        m_Root = m_Scene.NewSceneObject();
+        for (int x = -CUBES_XYZ[0]; x < CUBES_XYZ[0]; ++x) {
+            for (int y = -CUBES_XYZ[1]; y < CUBES_XYZ[1]; ++y) {
+                for (int z = -CUBES_XYZ[2]; z < CUBES_XYZ[2]; ++z) {
+                    auto so = m_Scene.NewSceneObject();
+                    so.SetParent(m_Root);
+                    m_Cubes.push_back(so);
+                }
+            }
+        }
     }
 
 protected:
     void ResetScene() {
-        m_CamPosition = { 0.f, 0.f, 0.f };
+        m_CamPosition = { 0.f, 0.f, -20.f };
         m_CamTarget = AVector3::Zero;
         m_CamUp = AVector3::Up;
 
-        m_Cube1.SetLocalTranslation({ 3.f, 0.f, 0.f });
-        m_Cube1.SetLocalScale(AVector3(0.5f));
-        m_Cube1.SetLocalRotation(AVector3::Zero);
-
-        m_Cube2.SetLocalTranslation({ 0.f, 1.0f, 0.f });
-        m_Cube2.SetLocalScale(AVector3(0.3f));
-        m_Cube2.SetLocalRotation(AVector3::Zero);
+        m_Root.SetLocalScale({ 1.f, 1.f, 1.f });
+        m_Root.SetLocalTranslation({ 0.f, 0.f, 0.f });
+        m_Root.SetLocalRotation({ 0.f, 0.f, 0.f });
+        size_t i = 0;
+        for (int x = -CUBES_XYZ[0]; x < CUBES_XYZ[0]; ++x) {
+            for (int y = -CUBES_XYZ[1]; y < CUBES_XYZ[1]; ++y) {
+                for (int z = -CUBES_XYZ[2]; z < CUBES_XYZ[2]; ++z) {
+                    m_Cubes[i].SetLocalScale({ 0.5f, 0.5f, 0.5 });
+                    m_Cubes[i].SetLocalTranslation({ (float)x, (float)y, (float)z });
+                    m_Cubes[i].SetLocalRotation({ 0.f, 0.f, 0.f });
+                    ++i;
+                }
+            }
+        }
 
         m_CameraFOV = 45.f;
 
@@ -63,33 +86,43 @@ protected:
     virtual void Tick(float deltaTime) override {
         Application::Tick(deltaTime);
 
+        m_AppTime += deltaTime;
+
         constexpr float tau = 6.28318530718;
-        AVector3 r = m_Cube1.GetLocalRotation();
+        AVector3 r = m_Root.GetLocalRotation();
         r.x = WrapMinMax(r.x, 0.f, tau);
         r.y = WrapMinMax(r.y, 0.f, tau);
         r.z = WrapMinMax(r.z, 0.f, tau);
-        m_Cube1.SetLocalRotation(r);
+        m_Root.SetLocalRotation(r);
 
+        r = m_Root.GetLocalRotation();
         if (m_SpinCube) {
-            m_Cube1.SetLocalRotation(m_Cube1.GetLocalRotation() + AVector3(deltaTime * m_CubeRotationSpeed));
+            m_Root.SetLocalRotation(r += AVector3(deltaTime * m_CubeRotationSpeed));
         }
 
-        m_CamTarget = m_Cube1.GetLocalTranslation();
+        size_t i = 0;
+        for (int x = -CUBES_XYZ[0]; x < CUBES_XYZ[0]; ++x) {
+            for (int y = -CUBES_XYZ[1]; y < CUBES_XYZ[1]; ++y) {
+                for (int z = -CUBES_XYZ[2]; z < CUBES_XYZ[2]; ++z) {
+                    m_Cubes[i].SetLocalRotation((AVector3((float)x, (float)y, (float)z ) * AVector3(m_AppTime*m_CubeRotationSpeed)));
+                    ++i;
+                }
+            }
+        }
+
+        m_CamTarget = m_Root.GetLocalTranslation();
 
         m_Proj = AMatrix4x4::PerspectiveFOV(m_CameraFOV * Deg2Rad, (float)m_Width / (float)m_Height);
         m_View = AMatrix4x4::LookAt(m_CamPosition, m_CamTarget, m_CamUp);
         AMatrix4x4::Invert(m_View);
 
-        m_Cube1.Update();
-        m_Cube2.Update();
+        m_Scene.Update();
 
-        m_MatrixMVP = m_Cube1.GetModelNoUpdate() * (m_View *  m_Proj);
-        SetMVP(m_MatrixMVP.v);
-        RenderCube();
-
-        m_MatrixMVP = m_Cube2.GetModelNoUpdate() * (m_View *  m_Proj);
-        SetMVP(m_MatrixMVP.v);
-        RenderCube();
+        for (auto& so : m_Cubes) {
+            m_MatrixMVP = so.GetModelNoUpdate() * (m_View *  m_Proj);
+            SetMVP(m_MatrixMVP.v);
+            RenderCube();
+        }
     }
 
     virtual void GUI() override {
@@ -106,17 +139,17 @@ protected:
         // model
         ImGui::Separator();
         ImGui::Separator();
-        AVector3 pos = m_Cube1.GetLocalTranslation();
+        AVector3 pos = m_Root.GetLocalTranslation();
         ImGui::DragFloat3("Cube Pos",   (float*)&pos,    0.05f);
-        m_Cube1.SetLocalTranslation(pos);
+        m_Root.SetLocalTranslation(pos);
 
-        AVector3 rot = m_Cube1.GetLocalRotation() * AVector3(Rad2Deg);
+        AVector3 rot = m_Root.GetLocalRotation() * AVector3(Rad2Deg);
         ImGui::DragFloat3("Cube Rot",   (float*)&rot,               0.05f);
-        m_Cube1.SetLocalRotation(rot * AVector3(Deg2Rad));
+        m_Root.SetLocalRotation(rot * AVector3(Deg2Rad));
 
-        AVector3 sca = m_Cube1.GetLocalScale();
+        AVector3 sca = m_Root.GetLocalScale();
         ImGui::DragFloat3("Cube Scale", (float*)&sca,       0.05f);
-        m_Cube1.SetLocalScale(sca);
+        m_Root.SetLocalScale(sca);
 
         ImGui::Checkbox("Spin Cube",    &m_SpinCube);
         ImGui::DragFloat("Spin Speed",  &m_CubeRotationSpeed,       0.001f);
